6962c1c37ca5926b7b03eafa7f895fdb8fbab992
Julius Caesar <ddcadr@gmail.com>
2024-08-19 11:03:30 -0300

    t

--- server/server/app.js
+++ server/server/app.js
@@ -8,7 +8,7 @@ const autenticado = require('../middleware/autenticado.js')
 
 const allowCors = (req, res, next) => {
     res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://piloto-client.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
     res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
     
